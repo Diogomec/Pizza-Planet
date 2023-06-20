017e8a193c9436be35e7f923900852130bc496e9
Diogo Parreira <diogomiguel.nunes@hotmail.com>
2023-06-20 18:47:16 +0100

    First Changes

---
+++ second-project/models/Pizza.model.js
@@ -0,0 +1,27 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: false,
+      unique: true
+    },
+    sauce: String,
+    ingredients: [String],
+    size: {
+      type: String,
+      default: "medium"
+    }
+    price: Number,
+  },
+  {
+    // this second object adds extra properties: `createdAt` and `updatedAt`    
+    timestamps: true
+  }
+);
+
+const User = model("User", userSchema);
+
+module.exports = User;

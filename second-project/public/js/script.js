8e78d1a9fd10143969a799dc7683235aa89e6983
Bernard Cardoso <be1.0@hotmail.com>
2023-06-24 00:21:56 +0100

    a few updates in CSS

--- second-project/public/js/script.js
+++ second-project/public/js/script.js
@@ -2,3 +2,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("second-project JS imported successfully!");
 });
+
+// Changing the cursor of the application
+document.addEventListener('mousemove', function(event) {
+  // Get the mouse coordinates
+  var mouseX = event.clientX;
+  var mouseY = event.clientY;
+
+  // Update the position of the pizza cursor
+  var pizzaCursor = document.getElementById('pizza-cursor');
+  pizzaCursor.style.left = (mouseX - 25) + 'px';
+  pizzaCursor.style.top = (mouseY - 25) + 'px';
+});
